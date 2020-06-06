19b642e4479e7f4b2df7b50c6ed5d83ecc26d9cb
hancock <ice.frozen.bai@outlook.com>
2020-06-07 00:00:04 +0800

    init-test Text component

--- SwiftUI-demo/ContentView.swift
+++ SwiftUI-demo/ContentView.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+            .foregroundColor(Color.red)
+            .font(Font.headline)
+            .bold()
+            .underline()
     }
 }
 
