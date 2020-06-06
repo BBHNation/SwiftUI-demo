ade366cf1648814a12afb2c6783bc7a1abcdaf35
hancock <ice.frozen.bai@outlook.com>
2020-06-06 23:45:58 +0800

    init project

---
+++ SwiftUI-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-demo
+//
+//  Created by Hancock on 2020/6/6.
+//  Copyright © 2020 hancock. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
